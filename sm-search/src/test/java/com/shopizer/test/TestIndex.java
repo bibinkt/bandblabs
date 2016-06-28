375cb326809d54c7ed5fcd0ff646a6bf63363ca0
bkthiruv <bkthiruv@jcp.com>
2016-06-28 23:47:24 +0530

    initial version

---
+++ sm-search/src/test/java/com/shopizer/test/TestIndex.java
@@ -0,0 +1,59 @@
+package com.shopizer.test;
+
+import javax.inject.Inject;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.shopizer.search.services.SearchService;
+
+
+@ContextConfiguration(locations = {
+		"classpath:spring/spring-context-test.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+	DependencyInjectionTestExecutionListener.class
+})
+
+/**
+ * Test if the server is running
+ * curl -X GET 'http://localhost:9200'
+ * 
+ * Test if an indice exists
+ * curl -XHEAD -i 'http://localhost:9200/product_en_default'
+ * 
+ * Get index infos
+ * curl -XGET 'http://localhost:9200/product_en_default/'
+ * 
+ * Get all indexed data
+ * curl -XGET 'http://localhost:9200/product_en_default/_search?pretty=1' 
+ * 
+ * @author carlsamson
+ *
+ */
+public class TestIndex {
+	
+	@Inject
+	private SearchService searchService;
+	
+	@Test
+	//@Ignore
+	public void testIndex() throws Exception {
+		
+		String jsonData = "{\"id\":\"1\",\"name\":\"Spring in action\",\"price\":\"23.99\",\"categories\":[\"book\",\"technology\"],\"store\":\"default\",\"availability\":\"*\",\"available\":\"true\",\"lang\":\"en\",\"description\":\"Best spring book, covers Spring MVC and Spring security\", \"tags\":[\"Spring\",\"Security\",\"Spring MVC\",\"Web\"]}";
+		//String jsonData = "{\"id\":\"2\",\"name\":\"Citizen tech model silver watch\",\"price\":\"453.99\",\"categories\":[\"Men watches\"],\"store\":\"default\",\"availability\":\"*\",\"available\":\"true\",\"lang\":\"en\",\"description\":\"Silver watch 2012 model\", \"tags\":[\"Silver watch\",\"Liquidation\"]}";
+		//String jsonData2 = "{\"id\":\"3\",\"name\":\"Citizen tech model black watch\",\"price\":\"483.99\",\"categories\":[\"Men watches\"],\"store\":\"default\",\"availability\":\"*\",\"available\":\"true\",\"lang\":\"en\",\"description\":\"Black watch 2012 model\", \"tags\":[\"Black watch\",\"Liquidation\"]}";
+		
+		searchService.index(jsonData, "product_en_default", "product_en");
+		
+		System.out.println("Done !");
+		
+	}
+
+}
---
+++ sm-search/src/test/java/com/shopizer/test/TestSearch.java
@@ -0,0 +1,77 @@
+package com.shopizer.test;
+
+import javax.inject.Inject;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.shopizer.search.services.SearchRequest;
+import com.shopizer.search.services.SearchResponse;
+import com.shopizer.search.services.SearchService;
+
+/**
+ * search api
+ * 
+ * Get all indexed data
+ * curl -XGET 'http://localhost:9200/product_en_default/_search?pretty=1' 
+ * 
+ * Search
+ * curl -XGET 'http://localhost:9200/product_en_default/_search' -d '{"query":{"query_string":{"fields" : ["name^5", "description", "tags"], "query" : "*spr*", "use_dis_max" : true }},"facets" : { "categories" : { "terms" : {"field" : "categories"}}}}'
+ * 
+ * @author carlsamson
+ *
+ */
+@ContextConfiguration(locations = {
+		"classpath:spring/spring-context-test.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+	DependencyInjectionTestExecutionListener.class,
+})
+
+
+public class TestSearch {
+	
+	@Inject
+	private SearchService searchService;
+	
+	@Test
+	//@Ignore
+	public void testSearch() throws Exception {
+		
+
+		String facets = "\"facets\" : { \"categories\" : { \"terms\" : {\"field\" : \"categories\"}}}";
+		
+		String q ="{\"query\":{\"query_string\":{\"fields\" : [\"name^5\", \"description\", \"tags\"], \"query\" : \"*spr*\", \"use_dis_max\" : true }}";
+
+		String query = q + "," + facets + "}";
+		
+		System.out.println(query);
+
+		//String json ="{\"query\":{\"filtered\":{\"query\":{\"text\":{\"_all\":\"beach\"}},\"filter\":{\"numeric_range\":{\"age\":{\"from\":\"22\",\"to\":\"45\",\"include_lower\":true,\"include_upper\":true}}}}},\"highlight\":{\"fields\":{\"description\":{}}},\"facets\":{\"tags\":{\"terms\":{\"field\":\"tags\"}}}}";
+
+		SearchRequest request = new SearchRequest();
+		request.addCollection("product_en_default");
+		request.setJson(query);
+		request.setSize(20);
+		request.setStart(0);
+		
+		
+		System.out.println(query);
+
+		SearchResponse resp= searchService.search(request);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String indexData = mapper.writeValueAsString(resp);
+		System.out.println("hiiiiiiiiiiiii"+resp.getCount());
+		
+		
+	}
+
+}
